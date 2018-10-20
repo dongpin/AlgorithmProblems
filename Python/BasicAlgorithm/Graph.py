84a3f9977b5413ba96d9b83c369ce8e295ac8612
dongpin <dongpin@hotmail.com>
2018-10-20 01:37:37 -0700

    Add graph representation and traverse

--- Python/BasicAlgorithm/Graph.py
+++ Python/BasicAlgorithm/Graph.py
@@ -1,6 +1,70 @@
 # Graph representations
+class Graph:
+    def __init__(self):
+        self.graph = {}
+    def add_edge(self, u, v):
+        if u in self.graph:
+            self.graph[u].append(v)
+        else:
+            self.graph[u] = [v]
+    def add_vertex(self, u):
+        if u not in self.graph:
+            self.graph[u] = []
 
-# DFS / BFS
+# Graph Traverse
+def dfs(graph):
+    def dfs_helper(graph, u, visited, res):
+        res.append(u)
+        visited.add(u)
+        if u in graph:
+            for v in graph[u]:
+                if v not in visited:
+                    dfs_helper(graph, v, visited, res)
+
+    visited = set()
+    res = []
+    for u in graph.keys():
+        if u not in visited:
+            dfs_helper(graph, u, visited, res)
+    return res
+
+def dfs_iterative(graph):
+    visited = set()
+    queue = []
+    res = []
+    for u in graph:
+        if u not in visited:
+            queue.append(u)
+            while queue:
+                cur = queue.pop()
+                if cur not in visited:
+                    visited.add(cur)
+                    res.append(cur)
+                if cur in graph:
+                    # add adjacent vertex in reverse order 
+                    # to maintain the same sequence as recursive approach
+                    for v in graph[cur][::-1]:
+                        if v not in visited:
+                            queue.append(v)
+    return res
+
+def bfs(graph):
+    visited = set()
+    stack = []
+    res = []
+    for u in graph:
+        if u not in visited:
+            stack.append(u)
+            while stack:
+                cur = stack.pop(0)
+                if cur not in visited:
+                    visited.add(cur)
+                    res.append(cur)
+                if cur in graph:
+                    for v in graph[cur]:
+                        if v not in visited:
+                            stack.append(v)
+    return res
 
 # Directed graph: Detect cycle, Topological sort, SCC, Kosaraju-Sharir algorithm
 
@@ -9,3 +73,27 @@
 # Shortest paths: Dijkstra's algorithm, Bellman-Ford algorithm, Negative cycle detection
 
 # Netword flow: max flow, min cut
+
+# Cycle detection
+
+# Topological sort
+
+# Minimum spanning tree
+
+# Shortest path algorithm
+
+# Flood-fill algorithm
+
+# Articulation point and bridges
+
+# Biconnected components
+
+# Strongly connected components
+
+# Hamiltonian path
+
+# Maximum path
+
+# Minimum cost maximum flow
+
+# Min-cut
---
+++ Tests/test_graph.py
@@ -0,0 +1,41 @@
+import unittest
+
+from Python.BasicAlgorithm.Graph import *
+
+def test_create_graph():
+    g = Graph()
+    g.add_edge(0, 1)
+    g.add_vertex(2)
+    g.add_edge(2,1)
+    assert g.graph[2] == [1]
+    assert g.graph.get(1) == None
+
+test_graph1 = {'A': ['B', 'C'], \
+              'B': ['E', 'F', 'G'], \
+              'C': ['E', 'G', 'K'], \
+              'K': ['I', 'J'], \
+              'J': ['A'], \
+              'Z': []}
+test_graph2 = {'A': [], \
+              'B': [], \
+              'C': []}
+test_graph3 = {'A': ['B', 'C']}
+test_graph4 = {}
+
+def test_dfs():
+    assert dfs(test_graph1) == ['A', 'B', 'E', 'F', 'G', 'C', 'K', 'I', 'J', 'Z']
+    assert dfs(test_graph2) == ['A', 'B', 'C']
+    assert dfs(test_graph3) == ['A', 'B', 'C']
+    assert dfs(test_graph4) == []
+
+def test_dfs_iterative():
+    assert dfs_iterative(test_graph1) == ['A', 'B', 'E', 'F', 'G', 'C', 'K', 'I', 'J', 'Z']
+    assert dfs_iterative(test_graph2) == ['A', 'B', 'C']
+    assert dfs_iterative(test_graph3) == ['A', 'B', 'C']
+    assert dfs_iterative(test_graph4) == []
+
+def test_bfs():
+    assert bfs(test_graph1) == ['A', 'B', 'C', 'E', 'F', 'G', 'K', 'I', 'J', 'Z']
+    assert bfs(test_graph2) == ['A', 'B', 'C']
+    assert bfs(test_graph3) == ['A', 'B', 'C']
+    assert bfs(test_graph4) == []
