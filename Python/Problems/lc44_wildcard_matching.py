35b782910aaac8a8a3813432badb9404875905cf
dongpin <dongpin@hotmail.com>
2019-08-24 22:35:38 -0700

    44. wildcard match

--- Python/Problems/lc44_wildcard_matching.py
+++ Python/Problems/lc44_wildcard_matching.py
@@ -1,11 +1,48 @@
 # 44. Wildcard Matching
 
-def is_match(s, p):
+def is_wildcard_match(s, p):
+
+    def match(s, p):
+
+        if (s, p) in memo:
+            return memo[(s, p)]
+
+        if s == p or p == '*':
+            memo[(s, p)] = True
+        elif p == '' or s == '':
+            memo[(s, p)] = False
+        elif p[0] == s[0] or p[0] == '?':
+            memo[(s, p)] = match(s[1:], p[1:])
+        elif p[0] == '*':
+            memo[(s, p)] = match(s[1:], p) or match(s, p[1:])
+        else:
+            memo[(s, p)] = False
+        return memo[(s, p)]
+
+    def remove_duplicate_stars(p):
+        if p == '':
+            return p
+        i = 0
+        stack = []
+        while i < len(p):
+            if not (stack and stack[-1] == '*' and p[i] == '*'):
+                stack.append(p[i])
+            i += 1
+        return ''.join(stack)
+
+    p = remove_duplicate_stars(p)
+
+    memo = {}
+    return match(s, p)
+
+
+'''
+def is_wildcard_match(s, p):
     if p == s or p == '*':
         return True
     if not p and not s:
         return True
-    
+
     dp = [[False] * (len(s)+1) for _ in range(len(p)+1)]
 
     dp[0][0] = True
@@ -13,29 +50,30 @@ def is_match(s, p):
     for p_index in range(1, len(p)+1):
         if p[p_index-1] == '*':
             s_index = 1
-            # d[p_idx - 1][s_idx - 1] is a string-pattern match 
+            # d[p_idx - 1][s_idx - 1] is a string-pattern match
             # on the previous step, i.e. one character before.
             # Find the first idx in string with the previous math.
             while not dp[p_index-1][s_index-1] and s_index < len(s) + 1:
                 s_index += 1
-            # If (string) matches (pattern), 
+            # If (string) matches (pattern),
             # when (string) matches (pattern)* as well
             dp[p_index][s_index-1] = dp[p_index-1][s_index-1]
-            # If (string) matches (pattern), 
+            # If (string) matches (pattern),
             # when (string)(whatever_characters) matches (pattern)* as well
             while s_index < len(s) + 1:
-                d[p_index][s_index] = True
+                dp[p_index][s_index] = True
                 s_index += 1
         # the current character in the pattern is '?'
         elif p[p_index-1] == '?':
-            for s_index in range(1, len(s) + 1): 
-                dp[p_idx][s_idx] = dp[p_index-1][s_index-1] 
+            for s_index in range(1, len(s) + 1):
+                dp[p_index][s_index] = dp[p_index-1][s_index-1]
         # the current character in the pattern is not '*' or '?'
         else:
-            for s_index in range(1, len(s) + 1): 
+            for s_index in range(1, len(s) + 1):
                 # Match is possible if there is a previous match
                 # and current characters are the same
                 dp[p_index][s_index] = \
-                    dp[p_index-1][s_index-1] and p[p_index-1] == s[s_index-1]  
-                                                               
+                    dp[p_index-1][s_index-1] and p[p_index-1] == s[s_index-1]
+
         return dp[len(p)][len(s)]
+'''
--- Python/Tests/test_lc_1.py
+++ Python/Tests/test_lc_1.py
@@ -2,10 +2,18 @@ import unittest
 
 from Python.Problems.lc32_longest_valid_parentheses import *
 from Python.Problems.lc56_merge_intervals import *
+from Python.Problems.lc44_wildcard_matching import *
 
 # 32
 def test_longest_parentheses():
     assert find_longest_valid_parenthese(")()())") == 4
+
+# 44
+def test_wildcard_matching():
+    assert is_wildcard_match("aa", "*") == True
+    assert is_wildcard_match("cb", "?a") == False
+    assert is_wildcard_match("adceb", "*a*b") == True
+    assert is_wildcard_match("acdcb", "a*c?b") == False
 
 # 56
 def test_merge_intervals():
